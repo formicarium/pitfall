fadf90121cf1f54f9fef276ac83ee65f36430410
Rafael Leal <rafaelealdias@gmail.com>
2020-06-01 10:55:07 -0300

    Remove soil reference

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [leoiacovini/clj-service "1.3.0"]]
 
-  :profiles {:dev     {:aliases      {"run-dev" ["trampoline" "run" "-m" "soil.server/run-dev"]}
+  :profiles {:dev     {:aliases      {"run-dev" ["trampoline" "run" "-m" "pitfall.server/run-dev"]}
                        :plugins      [[lein-midje "3.2.1"]]
                        :dependencies [[midje "1.9.1"]
                                       [clj-http-fake "1.0.3"]
