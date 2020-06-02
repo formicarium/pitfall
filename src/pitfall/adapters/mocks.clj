de6a58dfa7467bb61fa10dce19027f7ab11f5eba
Rafael Leal <rafaelealdias@gmail.com>
2020-06-02 18:31:00 -0300

    Try9

--- src/pitfall/adapters/mocks.clj
+++ src/pitfall/adapters/mocks.clj
@@ -1,7 +1,17 @@
 (ns pitfall.adapters.mocks
   (:require [pitfall.logic.mock :as logic.mock]
-            ))
+            [cheshire.core :as json]))
 
 (defn mock-opts
   [{:keys [request-method uri server-name] :as _context}]
   (merge {:method (name request-method) :route uri} (logic.mock/hostname-info server-name)))
+
+(defn internalize-mocks-config-map [config-map]
+  (update config-map
+          :data (fn [data]
+                  (into {} (map (fn [[k v]] [k (json/parse-string v true)]) data)))))
+
+(defn externalize-mocks-config-map [config-map]
+  (update config-map
+          :data (fn [data]
+                  (into {} (map (fn [[k v]] [k (if (string? v) v (json/generate-string v {:pretty true}))]) data)))))
--- src/pitfall/diplomat/kubernetes.clj
+++ src/pitfall/diplomat/kubernetes.clj
@@ -3,6 +3,7 @@
             [io.pedestal.log :as log]
             [cheshire.core :as json]
             [clj-service.protocols.config :as config]
+            [pitfall.adapters.mocks :as adapters.mocks]
             [pitfall.components.kubernetes :as components.kubernetes]))
 
 (defn get-mocks-config-map [devspace {:keys [kubernetes config]}]
@@ -13,16 +14,29 @@
                                      :action :get
                                      :request {:name      config-map-name
                                                :namespace devspace}})]
-    (log/info :config-map config-map)
+    (log/debug :config-map config-map)
     config-map))
 
 (defn get-mocks [devspace components]
   (->> (get-mocks-config-map devspace components)
-       :data
-       (map (fn [[k v]] [k (json/parse-string v true)]))
-       (into {})))
+       adapters.mocks/internalize-mocks-config-map
+       :data))
+
+(defn update-mocks-config-map [config-map devspace {:keys [kubernetes config]}]
+  (let [config-map-name (or (config/get-maybe config :mocks-config-map-name) "fmc-mocks")
+        _               (log/info :log :update-config-map :devspace devspace :config-map-name config-map-name)
+        config-map      (k8s/invoke (components.kubernetes/client kubernetes)
+                                    {:kind :ConfigMap
+                                     :action :update
+                                     :request {:name      config-map-name
+                                               :namespace devspace
+                                               :request   config-map}})]
+    (log/debug :config-map config-map)
+    config-map))
 
 (defn patch-mocks
-  [devspace data {:keys [kubernetes] :as components}]
-  (->> (update (get-mocks-config-map devspace components) :data #(merge % data))
-       (k8s/invoke (components.kubernetes/client kubernetes))))
+  [devspace data components]
+  (-> (get-mocks-config-map devspace components)
+      (update :data #(merge % data))
+      adapters.mocks/externalize-mocks-config-map
+      (update-mocks-config-map devspace components)))
