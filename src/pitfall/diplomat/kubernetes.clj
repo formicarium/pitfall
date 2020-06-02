73ae6d082e3e57ec8494176b9466335d2ded4c65
Rafael Leal <rafaelealdias@gmail.com>
2020-06-02 14:01:14 -0300

    Try2

--- src/pitfall/diplomat/kubernetes.clj
+++ src/pitfall/diplomat/kubernetes.clj
@@ -1,15 +1,24 @@
 (ns pitfall.diplomat.kubernetes
   (:require [kubernetes-api.core :as k8s]
             [cheshire.core :as json]
+            clojure.pprint
             [clj-service.protocols.config :as config]
             [pitfall.components.kubernetes :as components.kubernetes]))
 
-(defn get-mocks [devspace {:keys [kubernetes config]}]
+(defn tap [x]
+  (clojure.pprint/pprint x)
+  x)
+
+(defn get-mocks-config-map [devspace {:keys [kubernetes config]}]
   (->> {:kind :ConfigMap
         :action :get
         :request {:name      (or (config/get-maybe config :mocks-configmap-name) "fmc-mocks")
                   :namespace devspace}}
        (k8s/invoke (components.kubernetes/client kubernetes))
+       tap))
+
+(defn get-mocks [devspace components]
+  (->> (get-mocks-config-map devspace components)
        :data
        (map (fn [[k v]] [k (json/parse-string v true)]))
        (into {})))
